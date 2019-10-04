1c84bbc755c15d69565d9928e195774e8247e376
Brittany Houtz <brittany.houtz@gmail.com>
2019-10-04 11:09:48 -0700

    add updates for resubmission

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 import checkResult from './check-result.js';
+import getRandomThrow from './get-random-throw.js';
 
 // initialize/fetch DOM elements
 const shootButton = document.getElementById('shoot-button');
@@ -6,9 +7,8 @@ const totalSpan = document.getElementById('total');
 const winSpan = document.getElementById('win');
 const loseSpan = document.getElementById('lose');
 const drawSpan = document.getElementById('draw');
-const resultSpan = document.getElementById('result');
 const summarySpan = document.getElementById('summary');
-const rematchButton = document.getElementById('replay-button');
+const computerChose = document.getElementById('computer-chose');
 
 // set some initial state
 let playerWins = 0;
@@ -17,18 +17,6 @@ let playerDraws = 0;
 let totalPlays = 0;
 
 // define DOM utility function
-const playAgain = () => {
-    totalSpan.textContent = totalPlays;
-    winSpan.textContent = playerWins;
-
-};
-
-const updateSpans = () => {
-    winSpan.textContent = playerWins;
-    loseSpan.textContent = playerLosses;
-    drawSpan.textContent = playerDraws;
-    totalSpan.textContent = totalPlays;
-};
 
 // const playAgain = () => {
 //     playerWins = 0;
@@ -41,48 +29,32 @@ const updateSpans = () => {
         // grab values
 
 const selectedRadioImage = document.querySelector('input:checked');
-const userSelectedImage = selectedRadioImage.value;
+const playerChoice = selectedRadioImage.value;
 
        // make changes to state and dom
-totalPlays++;
-const randomNumber = getRandomThrow();
-const computerChoice = getComputerChoice(randomNumber);
-const win = playerWon(playerChoice, computerChoice);
-const lose = playerLost(playerChoice, computerChoice);
-const draw = itsADraw(playerChoice, computerChoice);
-
-if (win) {
-    playerWins++;
-} if (lose) {
-    playerLosses++;
-} if (draw) {
-    itsADraw++;
-}
-
-let result = checkResult(playerChoice, computerChoice);   
-
-if (result === 'win') {
-    gameResults.textContent = 'You Won!';
-} else if (result === 'lose'){
-    gameResults.textContent = 'You Lost!';
-} else if (result === 'draw'){
-    gameResults.textContent = 'It\'s a Draw!';
-}
+const resultChecker = () => {
+    const computerChoice = getRandomThrow();
+    const result = checkResult(playerChoice, computerChoice);   
+    computerChose.textContent = computerChoice;
+    if (result === 'win') {
+        summarySpan.textContent = 'You Won!';
+        playerWins++;
+    } else if (result === 'lose'){
+        summarySpan.textContent = 'You Lost!';
+        playerLosses++;
+    } else if (result === 'draw'){
+        summarySpan.textContent = 'It\'s a Draw!';
+        playerDraws++;
+    }
+    winSpan.textContent = playerWins;
+    loseSpan.textContent = playerLosses;
+    drawSpan.textContent = playerDraws;
+    totalSpan.textContent = totalPlays;
+};
 
 // // add event listeners
-
-
 shootButton.addEventListener('click', () => {
-    const computerChoice = getRandomThrow();
-    totalPlays = totalPlays + 1;
-    playerWins = playerWins + 1;
-    playerLosses = playerLosses + 1;
-    playerDraws = playerDraws + 1;
-    
-    totalSpan.textContent = totalPlays;
-    winSpan.textContent = totalPlays;
-    loseSpan.textContent = totalPlays;
-    drawSpan.textContent = totalPlays;
+    totalPlays++;
+    resultChecker();
 
-    // compare throws and determine winner??
 });
